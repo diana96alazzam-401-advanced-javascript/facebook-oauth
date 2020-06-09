4d064bed45dcd30ea8bf5ae0a09b98fb8e836a7c
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-09 09:34:03 +0300

    test1

--- public/facebookscript.js
+++ public/facebookscript.js
@@ -4,6 +4,7 @@ document.getElementById('loginbtn').addEventListener('click', loginWithFacebook,
 
 function loginWithFacebook(){
     FB.login(response => {
+        console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiii', response)
         const {authRespose:{accessToken, userID}} = response;
         fetch('/login-with-facebook', {
             method: 'POST',
